cfa266eb882be950067ae5a0cc32e9bce96ada28
khalilchatoo <chatookr@mcmaster.ca>
2017-09-08 16:20:12 -0400

    added alt to img, still need to separate css from components

--- src/components/TrendRow.js
+++ src/components/TrendRow.js
@@ -11,7 +11,7 @@ class TrendRow extends Component {
             <div style={{ clear: "both", overflow: "auto", padding: 15, borderBottom: "1px solid black" }}>
                 <h1 style={{ float: "left", color: "#AAAAAA" }}>{this.props.index} </h1>
                 
-                <img style={{ float: "left", marginLeft: 10, height: "100px", width: "100px", border: "1px solid black"}} src={this.props.picture}></img>
+                <img style={{ float: "left", marginLeft: 10, height: "100px", width: "100px", border: "1px solid black"}} src={this.props.picture} alt={this.props.title}></img>
 
                 <div style={{ float: "left", padding: 10 }}>
                     <div style={{ fontSize: 25, fontWeight: "bold" }}>{this.props.title}</div>
